e0a22e1bd47b4913b6e2e7aa1b91d6c3747ddca5
Michael Bowen <mikee.bowen@gmail.com>
2020-06-19 16:56:35 -0700

    seperate database and app into different projects with repository in between

--- CricketCreations/Controllers/BlogPostsController.cs
+++ CricketCreations/Controllers/BlogPostsController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CricketCreations.Data;
-using CricketCreations.Models;
 
 namespace CricketCreations.Controllers
 {
@@ -14,97 +12,97 @@ namespace CricketCreations.Controllers
     [ApiController]
     public class BlogPostsController : ControllerBase
     {
-        private readonly CricketCreationsContext _context;
+        //private readonly CricketCreationsContext _context;
 
-        public BlogPostsController(CricketCreationsContext context)
-        {
-            _context = context;
-        }
+        //public BlogPostsController(CricketCreationsContext context)
+        //{
+        //    _context = context;
+        //}
 
-        // GET: api/BlogPosts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPost()
-        {
-            return await _context.BlogPost.ToListAsync();
-        }
+        //// GET: api/BlogPosts
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPost()
+        //{
+        //    return await _context.BlogPost.ToListAsync();
+        //}
 
-        // GET: api/BlogPosts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
-        {
-            var blogPost = await _context.BlogPost.FindAsync(id);
+        //// GET: api/BlogPosts/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
+        //{
+        //    var blogPost = await _context.BlogPost.FindAsync(id);
 
-            if (blogPost == null)
-            {
-                return NotFound();
-            }
+        //    if (blogPost == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return blogPost;
-        }
+        //    return blogPost;
+        //}
 
-        // PUT: api/BlogPosts/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
-        {
-            if (id != blogPost.Id)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/BlogPosts/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
+        //{
+        //    if (id != blogPost.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(blogPost).State = EntityState.Modified;
+        //    _context.Entry(blogPost).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BlogPostExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!BlogPostExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/BlogPosts
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
-        {
-            _context.BlogPost.Add(blogPost);
-            await _context.SaveChangesAsync();
+        //// POST: api/BlogPosts
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPost]
+        //public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
+        //{
+        //    _context.BlogPost.Add(blogPost);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetBlogPost", new { id = blogPost.Id }, blogPost);
-        }
+        //    return CreatedAtAction("GetBlogPost", new { id = blogPost.Id }, blogPost);
+        //}
 
-        // DELETE: api/BlogPosts/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<BlogPost>> DeleteBlogPost(int id)
-        {
-            var blogPost = await _context.BlogPost.FindAsync(id);
-            if (blogPost == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/BlogPosts/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<BlogPost>> DeleteBlogPost(int id)
+        //{
+        //    var blogPost = await _context.BlogPost.FindAsync(id);
+        //    if (blogPost == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.BlogPost.Remove(blogPost);
-            await _context.SaveChangesAsync();
+        //    _context.BlogPost.Remove(blogPost);
+        //    await _context.SaveChangesAsync();
 
-            return blogPost;
-        }
+        //    return blogPost;
+        //}
 
-        private bool BlogPostExists(int id)
-        {
-            return _context.BlogPost.Any(e => e.Id == id);
-        }
+        //private bool BlogPostExists(int id)
+        //{
+        //    return _context.BlogPost.Any(e => e.Id == id);
+        //}
     }
 }
---
+++ CricketCreationsDatabase/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CricketCreationsDatabase.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        [Required]
+        [MaxLength(200)] 
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public List<BlogPost> BlogPosts { get; set; } = new List<BlogPost>();
+    }
+}
