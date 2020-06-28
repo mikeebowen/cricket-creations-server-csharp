09c601410a39f8b7aa72dfbf8c31f1b989dd461d
Michael Bowen <mikee.bowen@gmail.com>
2020-06-28 11:41:50 -0700

    add User model to all add, db and repo

---
+++ CricketCreationsRepository/Models/UserDTO.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CricketCreationsDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace CricketCreationsRepository.Models
+{
+    public class UserDTO
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public List<BlogPostDTO> BlogPosts { get; set; } = new List<BlogPostDTO>();
+        private static MapperConfiguration config = new MapperConfiguration(c => c.CreateMap<User, UserDTO>().ReverseMap());
+        private static IMapper mapper = config.CreateMapper();
+        public static List<UserDTO> GetAll()
+        {
+            return DatabaseManager.Instance.User.Select(u => mapper.Map<User, UserDTO>(u)).ToList();
+        }
+    }
+}
