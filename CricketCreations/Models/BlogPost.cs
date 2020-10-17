9e56d5c01da55a6e0372b40f6de84c2c9b569bc2
Michael Bowen <mikee.bowen@gmail.com>
2020-10-17 13:23:13 -0700

    Add BlogPostController and GetAll for BlogPosts

--- CricketCreations/Models/BlogPost.cs
+++ CricketCreations/Models/BlogPost.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using CricketCreationsRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,24 @@ namespace CricketCreations.Models
 {
     public class BlogPost
     {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        //public User User { get; set; }
+        public int UserId { get; set; }
+        private static MapperConfiguration config = new MapperConfiguration(c => c.CreateMap<BlogPost, BlogPostDTO>().ReverseMap());
+        private static IMapper mapper = config.CreateMapper();
+        public static async Task<List<BlogPost>> GetAll()
+        {
+            List<BlogPostDTO> blogPostDTOs = await BlogPostDTO.GetAll();
+            return blogPostDTOs.Select(b => convertToBlogPost(b)).ToList();
+        }
+        private static BlogPost convertToBlogPost(BlogPostDTO blogPostDTO)
+        {
+            return mapper.Map<BlogPostDTO, BlogPost>(blogPostDTO);
+        }
     }
 }
--- CricketCreationsRepository/Models/BlogPostDTO.cs
+++ CricketCreationsRepository/Models/BlogPostDTO.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using AutoMapper;
+using CricketCreationsDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CricketCreationsRepository.Models
 {
     public class BlogPostDTO
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string Image { get; set; }
+        //public UserDTO User { get; set; }
+        public int UserId { get; set; }
+        private static MapperConfiguration config = new MapperConfiguration(c => c.CreateMap<BlogPostDTO, BlogPost>().ReverseMap());
+        private static IMapper mapper = config.CreateMapper();
+        public static async Task<List<BlogPostDTO>> GetAll()
+        {
+            List<BlogPost> blogPosts = await DatabaseManager.Instance.BlogPost.ToListAsync();
+            return blogPosts.Select(b=> convertToBlogPostDTO(b)).ToList();
+        }
+        private static BlogPostDTO convertToBlogPostDTO(BlogPost blogPost)
+        {
+            return mapper.Map<BlogPost, BlogPostDTO>(blogPost);
+        }
     }
 }
