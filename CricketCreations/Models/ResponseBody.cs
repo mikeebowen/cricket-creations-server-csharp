65cfae8e6af886b91132919543819b5c4fd75a04
Michael Bowen <mikee.bowen@gmail.com>
2020-10-18 13:06:38 -0700

    Add ResponseBody class to follow JSON API Specifications

---
+++ CricketCreations/Models/ResponseBody.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CricketCreations.Models
+{
+    public class ResponseBody<T>
+    {
+        T t;
+        string type;
+        public ResponseBody(T typ, string s = null)
+        {
+            t = typ;
+            type = s;
+        }
+        public string Id
+        {
+            get
+            {
+                Type tp = t.GetType();
+                PropertyInfo prop = tp.GetProperty("Id");
+                string val = prop != null ? prop.GetValue(t).ToString() : null;
+                return val;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return type ?? typeof(T).ToString();
+            }
+        }
+        public T Data
+        {
+            get
+            {
+                return t;
+            }
+        }
+        public T Errors { get; set; }
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented, new JsonSerializerSettings { 
+                NullValueHandling = NullValueHandling.Ignore, 
+                ContractResolver = new CamelCasePropertyNamesContractResolver() 
+            });
+        }
+    }
+}
