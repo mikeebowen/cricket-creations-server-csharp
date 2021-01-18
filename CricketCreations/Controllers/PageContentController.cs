db79bdfbbce0036ac858b7314303572234c383ed
Michael Bowen <mikee.bowen@gmail.com>
2021-01-18 14:20:45 -0800

    Add PageContent to database and API to creat, read, update, and delete it

---
+++ CricketCreations/Controllers/PageContentController.cs
@@ -0,0 +1,116 @@
+﻿using CricketCreations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CricketCreations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PageContentController : ControllerBase
+    {
+        // GET: api/<PageContentController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<PageContent> pageContents = await PageContent.GetAll();
+            string response = new ResponseBody<List<PageContent>>(pageContents, "BlogPosts", pageContents.Count).GetJson();
+            return Ok(response);
+        }
+
+        // GET api/<PageContentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PageContentController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] JsonElement json)
+        {
+            try
+            {
+                string jsonString = json.ToString();
+                NJsonSchema.JsonSchema jsonSchema = NJsonSchema.JsonSchema.FromType<PageContent>();
+                ICollection<NJsonSchema.Validation.ValidationError> erros = jsonSchema.Validate(jsonString);
+
+                if (erros.Count == 0)
+                {
+                    PageContent pageContent = Newtonsoft.Json.JsonConvert.DeserializeObject<PageContent>(jsonString);
+                    pageContent.Created = DateTime.Now;
+                    pageContent.LastUpdated = pageContent.Created;
+                    PageContent content = await PageContent.Create(pageContent);
+                    string response = new ResponseBody<PageContent>(content, "PageContent", null).GetJson();
+                    return Created($"api/blogpost/{content.Id}", response);
+                }
+                else
+                {
+                    List<ErrorObject> errs = new List<ErrorObject>();
+                    erros.ToList().ForEach(e =>
+                    {
+                        errs.Add(new ErrorObject() { Message = e.Kind.ToString(), Property = e.Property });
+                    });
+                    return BadRequest(errs);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // PUT api/<PageContentController>/5
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Patch(int id, [FromBody] JsonElement json)
+        {
+            try
+            {
+                string jsonString = json.ToString();
+                NJsonSchema.JsonSchema jsonSchema = NJsonSchema.JsonSchema.FromType<PageContent>();
+                ICollection<NJsonSchema.Validation.ValidationError> erros = jsonSchema.Validate(jsonString);
+
+                if (erros.Count == 0)
+                {
+                    PageContent pageContent = Newtonsoft.Json.JsonConvert.DeserializeObject<PageContent>(jsonString);
+                    pageContent.LastUpdated = DateTime.Now;
+                    pageContent.Id = id;
+                    PageContent content = await PageContent.Update(pageContent);
+                    if (content != null)
+                    {
+                        string response = new ResponseBody<PageContent>(content, "BlogPost", null).GetJson();
+                        return Ok(response);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                else
+                {
+                    List<ErrorObject> errs = new List<ErrorObject>();
+                    erros.ToList().ForEach(e =>
+                    {
+                        errs.Add(new ErrorObject() { Message = e.Kind.ToString(), Property = e.Property });
+                    });
+                    return BadRequest(errs);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // DELETE api/<PageContentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
